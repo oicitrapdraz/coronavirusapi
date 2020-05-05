ad09be07f43795bc82a76310494af3aaaa39d2a8
Patricio Sard <patriciosard@gmail.com>
2020-05-04 22:53:57 -0400

    Add model scopes to DRY some code (don't repeat yourself) and at the same time remove string interpolation in some queries (it's dangerous since its open to SQL injection)

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,6 @@
-require 'csv'
+# frozen_string_literal: true
 
 class State < ApplicationRecord
+  scope :is_official, -> { where(official_flag: true) }
+  scope :is_official_and_name_is, ->(name) { is_official.where(name: name) }
 end
